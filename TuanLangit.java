a8901f116f40aeeffd617bac3b3b49e097cc3745
RioSudrajat <144890893+RioSudrajat@users.noreply.github.com>
2023-09-14 22:09:53 +0700

    Add files via upload

---
+++ TuanLangit.java
@@ -0,0 +1,22 @@
+public class TuanLangit {
+     public static void main(String[] args) {
+        String TuanLangit = ("\n Selamat datang di toko langit");
+        TuanLangit +=("\n =================================");
+        TuanLangit +=("\n mohon habiskan uang anda");
+        TuanLangit +=("\n uang anda habis, kami senang!");
+        TuanLangit +=("\n kamin menyediakan segala barang yg tidak pernah anda jumpai di darat");
+        TuanLangit +=("\n anda bisa cek daftar barang yang tersedia dibawah ini");
+        TuanLangit +=("\n &_&&_&&_&&_&&_&&_&&_&&_&&_&");
+        TuanLangit +=("\n 1. Selembar awan");
+        TuanLangit +=("\n 2. Kanvas cakrawala");
+        TuanLangit +=("\n 3. Tinta hujan");
+        TuanLangit +=("\n 4. Kuas senja");
+        TuanLangit +=("\n 5. Atap negeri");
+        TuanLangit +=("\n :):):):):)::):):):)");
+        TuanLangit +=("\n TERIMAKASIH TELAH BERBELANJA DI TOKO KAMI");
+        TuanLangit +=("\n jika gajian sudah cair, datanglah kembali");
+
+
+        System.out.println(TuanLangit);
+    }
+}
